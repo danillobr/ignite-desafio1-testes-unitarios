76e58f2ff4763420f565189bbedd95b939b58f15
danillobr <danillorodriguesabreu@gmail.com>
2022-12-20 10:40:35 -0300

    Teste unitários de CreateUserUseCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,8 +1,9 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
-let repository: InMemoryUsersRepository;
+let repository: IUsersRepository;
 let createUserUseCase: CreateUserUseCase;
 
 describe("Create a user", () => {
@@ -19,5 +20,21 @@ describe("Create a user", () => {
     });
 
     expect(user).toHaveProperty("id");
+  });
+
+  it("Should not be able create a user with an existent email", async () => {
+    expect(async () =>{
+      await createUserUseCase.execute({
+        name: "user1",
+        email: "email@test",
+        password: "test",
+      });
+  
+      const user = await createUserUseCase.execute({
+        name: "user2",
+        email: "email@test",
+        password: "test",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError)
   });
 });
